de47030d92d1af7f1141e6986f9e2d57c8a3bd39
AvrAlexandra <avramescu.alexandra@gmal.com>
2024-03-24 22:02:02 +0200

    Added navbar, right sidebar and profile picture components

--- family_hub_fe/src/app/app.module.ts
+++ family_hub_fe/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LeftSidebarComponent } from './components/left-sidebar/left-sidebar.component';
+import { ProfilePictureComponent } from './components/profile-picture/profile-picture.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LeftSidebarComponent,
+    ProfilePictureComponent
   ],
   imports: [
     BrowserModule,
