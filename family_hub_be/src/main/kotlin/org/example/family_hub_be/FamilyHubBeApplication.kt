24f21b5e431d282e8e32d614625ac52e527b24a1
ambragoanta <ambra.goanta@student.upt.ro>
2024-03-26 17:25:07 +0200

    setup backend

---
+++ family_hub_be/src/main/kotlin/org/example/family_hub_be/FamilyHubBeApplication.kt
@@ -0,0 +1,11 @@
+package org.example.family_hub_be
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FamilyHubBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<FamilyHubBeApplication>(*args)
+}
